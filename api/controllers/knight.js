a4731dc9dfa8ce73505510b2c119ac9d6f8423e6
Nikhil <nikhilcares22@gmail.com>
2022-09-02 17:46:44 +0530

    port added

--- api/controllers/knight.js
+++ api/controllers/knight.js
@@ -4,7 +4,7 @@ exports.predictMoves = async (req, res) => {
 	try {
 		let x = [2, 2, 1, 1, -2, -2, -1, -1]; //possible moves
 		let y = [1, -1, 2, -2, 1, -1, 2, -2]; //possible moves
-
+        let pos = req.body.pos;
 		// function predictMoves(pos, m, n) {
 		//where m and n represent the length in x and y axis resp.
 		if (!pos.hasOwnProperty("x") || !pos.hasOwnProperty("y"))
